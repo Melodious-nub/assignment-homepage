2b3a5a93d337cf92ce373d9b9c90ed4a9860f6e1
Melodious-nub <shawon.taluckder1@gmail.com>
2025-03-25 16:37:56 +0600

    updated with button color

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HeroComponent }
+];
